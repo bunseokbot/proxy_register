b75a9eab312a2ac787e4b0e44115f8bad4508418
bunseokbot <bunseokbot@gmail.com>
2017-08-20 00:34:10 +0900

    Add register, restart, write to handle nginx

--- route/__init__.py
+++ route/__init__.py
@@ -1,3 +1,10 @@
+from route.db import db_session
+from route.models import Domain
+
+import os
+import subprocess
+
+
 class Route(object):
     def __init__(self, ip, domain):
         self.ip = ip
@@ -6,13 +13,57 @@ class Route(object):
     def __str__(self):
         return self.domain
 
-    def query(self):
+    def search(self, option, value):
         """ search domain routing info """
-        pass
+        d = Domain
 
-    def register(self):
+        if option == "ip":
+            d.filter(Domain.ip == value)
+
+        elif option == "domain":
+            d.filter(Domain.domain == value)
+
+        elif option == "user":
+            d.filter(Domain.user == value)
+
+        return d.all()
+
+    def register(self, user):
         """ register domain nginx reverse proxy """
-        pass
+        self.write_file()
+        self.restart_nginx()
+
+        db_session.add(Domain(ip, domain, user))
+        db_session.commit()
+
+    def restart_nginx(self):
+
+
+    def write_file(self):
+        data = "server {\n"\
+            "\tlisten 443 ssl;\n"\
+            "\tserver_name {}\n\n".format(self.domain)\
+            "\tlocation / {\n"\
+            "\t\tproxy_redirect off;\n"\
+            "\t\tproxy_pass_header Server;\n"\
+            "\t\tproxy_set_header Host $http_host;\n"\
+            "\t\tproxy_set_header X-Real-IP $remote_addr;\n"\
+            "\t\tproxy_set_header X-Scheme $scheme;\n"\
+            "\t\tproxy_pass http://{};\n".format(self.ip)\
+            "\t}\n"\
+            "}\n\n"\
+            "server {\n"\
+            "\tlisten 80;\n"\
+            "\tserver_name {}\n\n".format(self.domain)\
+            "\tlocation / {\n"\
+            "\t\trewrite ^(.*) https://$host$1 permanent;\n"\
+            "\t}\n"\
+            "}\n"
+
+        with open("/etc/nginx/sites-available/{}".format(self.domain), "a") as f:
+            f.write(data)
+
+        os.symlink("/etc/nginx/sites-available/{}".format(self.domain), "/etc/nginx/sites-enabled/{}".format(self.domain))
 
     def __del__(self):
         del self
